1ca5ebfb2eef9a0d1a8e31010133feea7e97c3de
solsollala <hioh@naver.com>
2017-07-23 07:26:21 +0900

    vworld test

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
---
+++ run.bat
@@ -0,0 +1,3 @@
+cd C:\Users\JSOH\Desktop\PyCharm Projects\djangogirls
+python manage.py runserver
+cd C:\Users\JSOH\
