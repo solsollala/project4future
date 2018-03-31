1957e342317c60d3538c243ac107735d19fa319a
solsollala <hioh@naver.com>
2018-03-31 16:41:50 +0900

    VCS

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,6 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework_xml.renderers.XMLRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
