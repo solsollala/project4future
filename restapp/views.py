d0d5a7bf405dd06c39f776fccad66555103cc78d
solsollala <hioh@naver.com>
2018-12-01 12:23:34 +0900

    커밋
    커밋

--- restapp/views.py
+++ restapp/views.py
@@ -8,16 +8,14 @@ from rest_framework.decorators import api_view
 # ---------------------------------------------------------------------
 # Funtion based view 로 구현
 # ---------------------------------------------------------------------
-
 @api_view(['POST'])
 def sa_webhook(request):
     result = [ "{'status' : 200}" ]
     return Response(result)
 
-
 from django.http import JsonResponse
 
 @api_view(['POST'])
 def dialogflow(request):
-    r = {"speech":"고기없어요","displayText":"고기없어요","messages":{"type": 0,"speech": "요즘 이시간에는 고기없어요"},"data":{},"contextOut":[],"source":"example.com","followupEvent":{}}
+    r = {"speech":"고등어요","displayText":"고기없어요","messages":{"type": 0,"speech": "요즘 이시간에는 고기없어요"},"data":{},"contextOut":[],"source":"example.com","followupEvent":{}}
     return JsonResponse(r)
