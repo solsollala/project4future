810d7a057f6f9c32969eb1077a4ff5ff5bff0d15
solsollala <hioh@naver.com>
2018-12-01 12:06:06 +0900

    VCS

--- restapp/views.py
+++ restapp/views.py
@@ -19,5 +19,5 @@ from django.http import JsonResponse
 
 @api_view(['POST'])
 def dialogflow(request):
-    r = {"speech":"감성돔요","displayText":"감성돔요","messages":{"type": 0,"speech": "요즘 이시간에는 감성돔이 많이 나와요"},"data":{},"contextOut":[],"source":"example.com","followupEvent":{}}
+    r = {"speech":"고기없어요","displayText":"고기없어요","messages":{"type": 0,"speech": "요즘 이시간에는 고기없어요"},"data":{},"contextOut":[],"source":"example.com","followupEvent":{}}
     return JsonResponse(r)
